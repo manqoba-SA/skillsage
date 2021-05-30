96303101b86b70d4a7fd0ef319d079e411bde4c7
Manqoba Zwane <67443742+manqoba-SA@users.noreply.github.com>
2021-05-30 23:20:02 +0200

    Updated content on index.html

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>
-        Welcome!
+        Home
     </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -43,9 +43,9 @@
     <!-- ==================Page Nav ================== -->
     <div class="welcome-text">
         <h1>Skillshare Helps South Africans to Achirve their Dream!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quis voluptates officiis deleniti obcaecati debitis, ut, 
-            eveniet necessitatibus alias eos ab corrupti mollitia, neque ad provident voluptatibus sequi quae beatae?
-            orem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quis voluptates officiis deleniti obcaecati debitis, ut, 
+        <p>The main aim of this website is to help those who are less fortunate, by giving them 
+        second chances and opportunities to enhance their skills and better their future, 
+        in terms of job opportunities and school continuation. 
         </p>
         <div class="header-button">
         <a href="pages/register.php"><button class="register-button">Get Started</button></a>
@@ -59,33 +59,39 @@
 <section id="about" class="intro-page intro-page-js">
     <h1 class="section-heading">About Us - Why NanoBytes</h1>
     <p class="intro-text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-        excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+    We are a site that caters programs that are suited for any South African to up Skill or learn new skills, 
+    we offer rare skills that are in demand, you need no prior qualification to qualify for these programs only
+    a willing heart to learn and a mind that is ready. The purpose of SkillSAGE is to ensure the South African youth 
+    is well equipped with the necessary skills to meet the immediate demands by the 4th Industrial revolution, We as SkilsAge 
+    understand that The future of the south Africa belongs to the South African youth, and it is from the youth and not from 
+    the old that the fire of life will warm and enlighten the world. It is your privilege to breathe the breath of life into 
+    the dry bones of many around you.
     </p>
     <div class="intro-page-content">       
             <div class="row col-1">
                 <h3><span class="first-letter">P</span>overty</h3>
                 <div class="row-image"><img src="images/poor.jpg"></div>
-                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio! </p>
+                <p> Poverty still pervades South Africa, it exposes the cracks between us. SkillsAge wants to ensure that this no longer 
+                happens due to lack of skills for one to support him or her self, 
+                SkillsAge will provide the youth with the best skills to ensure that they can build the best future for themselves and others. </p>
             </div>
             <div class="row col-1">               
                 <h3><span class="first-letter">C</span>rime</h3>
                 <div class="row-image"><img src="images/crime.jpg"></div>
-                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio! </p>
+                <p>Crime Rates have always been high in South Africa, due to many reasons other than poverty or unemployment. 
+                   SkillsAge will ensure that the youth is equipped with the best skills to ensure that the youth generates a descent income. </p>
             </div>
             <div class="row col-1">
                 <h3><span class="first-letter">U</span>nemployment </h3>
                 <div class="row-image"><img src="images/South-Africa-Unemployment.jpeg"></div>
-                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio! </p>
+                <p>Unemployment, many things come from unemployment things such as Crime and Poverty, as SkillsAge we will ensure that this no longer happens, 
+                   we will equip the youth with the necessary skills for them to be able to get a Job or create Jobs for many</p>
             </div>
             <div class="row col-1">
                 <h3><span class="first-letter">D</span>ependency</h3>
                 <div class="row-image"><img src="images/file-20190621-61733-1xjhmz1.jpg"></div>
-                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio! </p>
+                <p>Dependency is one of the leading causes of poverty, we as SkillsAge 
+                we will ensure that a person doesn't have a singular source of income rather a multitude of income sources.</p>
             </div>   
     </div>
 </section>
@@ -99,8 +105,8 @@
         <div class="step-number step-1">
             <h3>Step 1</h3>
             <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+            The first step to do is to register with us and become a skillsAge member and get all the latest updates and all the information
+            you may need.
             </p>
         </div>
     </div>
@@ -108,8 +114,7 @@
         <div class="step-number step-2">
             <h3>Step 2</h3>
             <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+            second step is to login with the details you used to register and have an access to our skillsAge website to see all the info.
             </p>
         </div>
     </div>
@@ -117,8 +122,7 @@
             <div class="step-number step-3">
                 <h3>Step 3</h3>
                 <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+                    Once you are login you can now check for nofications to get all the latest updates from our website team.
                 </p>
             </div>
         </div>
@@ -126,8 +130,8 @@
             <div class="step-number step-4">
                 <h3>Step 4</h3>
                 <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+                    After you checked for notifications you can check also on blog post where you will get the latest new for opportunities and 
+                    other info related to opportunities.
                 </p>
             </div>
         </div>
@@ -135,8 +139,8 @@
             <div class="step-number step-5">
                 <h3>Step 5</h3>
                 <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-                    excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+                    Once you checked for both notifications and blogs now you can explore the website and choose the category you may fall for so you 
+                    can find information and opportunities.
                 </p>
             </div>
         </div>
@@ -149,8 +153,7 @@
 <section id="team" class="our-team">
     <h1 class="section-heading" >Our Team</h1>
     <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus provident rem a repudiandae magnam, culpa iure eaque, 
-        excepturi quo vero voluptatum dignissimos cupiditate sit eum veritatis, facere exercitationem repellat odio!
+    Get to know our team who are behind the SkillsAge Company.
     </p>
     <div class="team-cards">
         <div class="member-card">
@@ -160,45 +163,54 @@
             <h3>Manqoba Ngubeni</h3>
             <ul>
                 <li>Chief Exrcutive Officer</li>
-                <li>Email: nqobi@nanos.com</li>
-                <li>Cell: 0643216072</li>
+                <li>Email: nqobi.it4073@gmail.com</li>
+                <li>Cell: +27643216072</li>
             </ul>
         </div>
         <div class="member-card">
             <div class="member-image">
-                <img src="images/ineq1.jpg">
+                <img src="images/rea.jpeg">
             </div>
-            <h3>Rea</h3>
-            <p>Manoqoba is full stack developer in the class of BIT class of 2021 second year</p>
+            <h3>Reamohetse Sehlako</h3>
+            <ul>
+                <li>General Manager</li>
+                <li>Email: 220008907@student.uj.ac.za</li>
+                <li>Cell: +27769202579</li>
+            </ul>
         </div>
         <div class="member-card">
             <div class="member-image">
-                <img src="images/ineq1.jpg">
+                <img src="images/sabelo.jpeg">
             </div>
-            <h3>Sabza</h3>
-            <p>Manoqoba is full stack developer in the class of BIT class of 2021 second year</p>
+            <h3>Lerato Khumalo</h3>
+            <ul>
+                <li>Human Resource manager</li>
+                <li>Email: Klerato43@gmail.com</li>
+                <li>Cell: +27769202579</li>
+            </ul>
         </div>
         
         <div class="member-card">
             <div class="member-image">
-                <img src="images/ineq1.jpg">
+                <img src="images/siya.jpeg">
             </div>
-            <h3>Kg</h3>
-            <p>Manoqoba is full stack developer in the class of BIT class of 2021 second year</p>
+            <h3>Siyabonga Dhlamini</h3>
+             <ul>
+                <li>Project Manager</li>
+                <li>Email: dhlaminisiyabpnga626@gmail.com</li>
+                <li>Cell: +27845549276</li>
+            </ul>
         </div>
         <div class="member-card">
             <div class="member-image">
-                <img src="images/ineq1.jpg">
+                <img src="images/nhlanhla.jpeg">
             </div>
-            <h3>Siya</h3>
-            <p>Manoqoba is full stack developer in the class of BIT class of 2021 second year</p>
-        </div>
-        <div class="member-card">
-            <div class="member-image">
-                <img src="images/ineq1.jpg">
-            </div>
-            <h3>Nhlalala</h3>
-            <p>Manoqoba is full stack developer in the class of BIT class of 2021 second year</p>
+            <h3>Nhlalala Mabodze</h3>
+            <ul>
+                <li>Advisor</li>
+                <li>Email: mabodzenhlalala@gmail.com</li>
+                <li>Cell: +27823670925</li>
+            </ul>
         </div>
     </div>
 </section>
@@ -218,31 +230,60 @@
                     Un​iversity of Johannesburg​ PO Box 524 Auckland Park 2006 South Africa
                 </li>
             </ul>
+            
             <div class="contact-us-form">
+            
                 <h2>Send Message</h2>
-                <form class="contact-form">
+                <form class="contact-form" action="index.php" method="POST">
+                <?php 
+                if(isset($_POST['submit'])){
+                    $username = $_POST['name'];
+                    $email = $_POST['email'];
+                    $topic = $_POST['topic'];
+                    $message = $_POST['message'];
+                
+                    require_once("includes/dbh.php");
+                    require_once("includes/processes.php");
+
+                    if(emptyInputSignUp($username,$email,$topic,$message) !== false){
+                        echo "<h2>Please fill all the forms</h2>";
+                        
+                    }
+                    else{
+                        $sqlQuery = "INSERT INTO emails (name, email, topic, message) VALUES(?, ?, ?, ?);";
+                        $stmt = mysqli_stmt_init($conn);
+                        if(!mysqli_stmt_prepare($stmt, $sqlQuery)){
+                            echo "h2>SMTP failed</h2>";
+                        }
+                        mysqli_stmt_bind_param($stmt, "ssss", $name, $email, $topic, $message);
+                        mysqli_stmt_execute($stmt);
+                        mysqli_stmt_close($stmt);
+                        echo "<h2>Awez🤙 we received your email</h2>";
+                    }
+                }
+            ?>
                     <div class="form-field col-lg-6">
-                        <input id="name" class="input-text js-input" type="text" required>
+                        <input id="name" name="name" class="input-text js-input" type="text">
                         <span>Full name</span>   
                     </div>
 
                     <div class="form-field col-lg-6">
-                        <input id="name" class="input-text js-input" type="email" required>
+                        <input id="name" name="email" class="input-text js-input" type="email">
                         <span>Email</span>   
                     </div>
 
                     <div class="form-field col-lg-6">
-                        <input id="name" class="input-text js-input" type="text" required>
+                        <input id="name" name="topic" class="input-text js-input" type="text">
                         <span>Topic</span> 
                     </div>
 
                     <div class="form-field col-lg-6">
-                        <textarea id="name" class="input-text js-input" type="text" required></textarea>
+                        <textarea id="name" name="message" class="input-text js-input" type="text"></textarea>
                         <span>Message</span> 
                     </div>
 
                     <div class="form-field col-lg-6">
-                        <input type="submit" name="" value="Send"> 
+                        <input type="submit" name="submit" value="Send"> 
                     </div>
                 </form>
             </div>
@@ -268,8 +309,8 @@
             <h3>Legal and Privacy</h3>
             <ul>
                 <li><a href="pages/policy.php">Private Policy</a></li>
-                <li><a href="pages/policy.php">FAQs</a></li>
-                <li><a href="pages/policy">Tearms and Conditions</a></li>
+                <li><a href="pages/mainpages/faqs.php">FAQs</a></li>
+                <li><a href="pages/mainpages/terms-conditions.php">Tearms and Conditions</a></li>
                 <li><a href="#">Home</a></li>
             </ul>
         </div>
